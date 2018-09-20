8f9e08c88d27f21847b177a9dfd8504fab11a1ca
Alain Petit <alainpetit@hotmail.com>
2018-09-20 12:13:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+PROJECT : Dater
+
+Description : 
+Little project that read an directory structure in the form of YYYY/[MM/DD/]file and change each file creation date to the appropriate directory structure. 
+
+Over the years, I had my photo album collection move from one disk to another, but creation dates (for some reasons) do not follow, or in some cases, photo were edited to adjust color, format, etc. thus altering the modified date. This project override the file date to whatever is in the directory structure. 
+
+
+WHAT YOU NEED TO COMPILE :
+-Linux Code::Block 17.12 (however, you could probably compile it from command line under any linux and compiler
+
+
+
+Files ID :
+*.cbp : Code::block project
+*.depend,.layout : Code::block stuffs
+dater : latest build linux executable
+Dater
---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cstring>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include <utime.h>
+
+int
+main(int argc, char** argv)
+{
+	if(argc == 1){
+		printf("Wrong usage : dater \"YYYY/[MM/DD/]filename\"\n");
+		return 1;
+	}
+
+	int i;
+	for(i= 1; i < argc; ++i){
+		char	strYear[5];
+		char	strMonth[3];
+		char	strDay[3];
+		char	strHour[3];
+		char	strMin[3];
+		int 	nYear;
+		int 	nMonth	= 1;
+		int 	nDay	= 1;
+		int		nHour;
+		int		nMin;
+
+		printf("\n%s", argv[i]);
+
+
+		//Try to find the year directory this way : /19XX/
+		char *pt= &argv[i][0];
+		while(*pt){
+			if((pt[0] == '/') && (pt[1] == '1') && (pt[2] == '9'))
+				break;
+			if((pt[0] == '/') && (pt[1] == '2') && (pt[2] == '0'))
+				break;
+
+			pt++;
+		}
+		if(!*pt){
+			printf("Wrong directory struture ... your files must be in a /19YY/ or /20YY/ year directory");
+			return 1;
+		}
+
+		pt++;
+		memcpy(strYear, pt, 4);
+		strYear[4]= 0;
+		nYear= atoi(strYear);
+		pt+= 5;
+
+		//Try to find the month
+		if(pt[2] == '/'){
+			memcpy(strMonth, pt, 2);
+			strMonth[2]= 0;
+			nMonth= atoi(strMonth);
+			pt+= 3;
+
+			//Try to find the day
+			if(pt[2] == '/'){
+				memcpy(strDay, pt, 2);
+				strDay[2]= 0;
+				nDay= atoi(strDay);
+				pt+= 3;
+			}
+		}
+
+		//Get the Base info
+		struct tm* clock;					// create a time structure
+		struct stat attrib;					// create a file attribute structure
+
+		stat(argv[i], &attrib);				// get the attributes of afile.txt
+		clock= localtime(&(attrib.st_mtime));	// Get the last modified time and put it into the time structure
+
+		clock->tm_year= nYear - 1900;
+		clock->tm_mon= nMonth - 1;
+		clock->tm_mday= nDay;
+
+
+		struct utimbuf timeToWrite;
+
+		timeToWrite.actime = mktime(clock);
+		timeToWrite.modtime = mktime(clock);
+
+		utime(argv[i], &timeToWrite);
+	}
+
+	return 0;
+}
